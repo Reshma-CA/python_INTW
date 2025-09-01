f46ca68706306d4dc8e75202f90d2964677a4669
Reshma.CA <reshmacaci@gmail.com>
2025-09-01 16:04:59 +0530

    fourth commit

--- new_python.py
+++ new_python.py
@@ -76,39 +76,6 @@ print(new_data["departments"][1]["employees"][1]["role"])
 print(new_data["departments"][1]["employees"][0]["role"])
 
 
-str1 = "UPPU"
-print(str1.isupper()) # check string is upper case
-str2 = "lower"
-print(str2.islower()) # check string is lower case
-str3 = "Hai Myname Is Reshma" 
-print(str3.istitle()) # check string is title case
-str4 = "12546665" 
-print(str4.isdigit()) # check string is digit
-str5 = "Reshma658955"
-print(str5.isalnum()) # check string includes alphabet and numbers
-str6 = "GshgfshRTYUYIUIKJ"
-print(str6.isalpha()) # check string include both upper case and lower case letters
-str7 = " "
-print(str7.isspace())
-
-remove_space = " Reshma   "
-print("My name is ",remove_space.strip())
-
-str22 = "Hai world"
-new_str = str22.replace("Hai","Hello")
-print(new_str)
-
-new_strr = "Hello my name is Reshma"
-after_split = new_strr.split(" ")
-print(after_split)
-
-new_strrr = ("Hello","Python","world")
-after_joint = " ".join(new_strrr)
-print(after_joint)
-
-
-
-
 
 
 
